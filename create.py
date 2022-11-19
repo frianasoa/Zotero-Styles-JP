778759081d854c1a28737e8e19e11349c0158b55
frianasoa <frianasoa@gmail.com>
2022-11-19 17:43:36 +0900

    Edited macro issue

--- create.py
+++ create.py
@@ -1,3 +1,7 @@
 from scripts.chicago import Chicago
-c = Chicago()
+c = Chicago(
+        input  = 'scripts/source/chicago-author-date.csl', 
+        journal = "Africa Educational Research Journal", 
+        suffix = {"id": "aerj-ja", "title":"AERJ　（日本語）"}
+    )
 c.create()
