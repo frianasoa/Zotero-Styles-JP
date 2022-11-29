5f552c26f6cf19f1f321ac19865f884fe1ebaed4
frianasoa <frianasoa@gmail.com>
2022-11-29 21:44:00 +0900

    Set Chicago as a base class

--- create.py
+++ create.py
@@ -1,5 +1,5 @@
-from scripts.chicago import Chicago
-c = Chicago(
+from scripts.aerj import Aerj
+c = Aerj(
         input  = 'scripts/source/chicago-author-date.csl', 
         journal = "Africa Educational Research Journal", 
         suffix = {"id": "aerj-ja", "title":"AERJ　（日本語）"}
---
+++ scripts/aerj.py
@@ -0,0 +1,5 @@
+from .chicago import Chicago
+
+class Aerj(Chicago):
+    def __init__(self, input, journal, suffix):
+        super().__init__(input, journal, suffix)
