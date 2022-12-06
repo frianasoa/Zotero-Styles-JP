1b0a4114fcad51d043c80d8530ee3cb6763f69a7
frianasoa <frianasoa@gmail.com>
2022-12-06 21:36:06 +0900

    Adding Kyosei Studies Journal (stuck at delimiter between last name and first name)

--- create.py
+++ create.py
@@ -1,5 +1,6 @@
 from scripts.aerj import Aerj
 from scripts.jids import Jids
+from scripts.kyosei import Kyosei
 Aerj(
     input  = 'scripts/source/chicago-author-date.csl', 
     journal = "Africa Educational Research Journal", 
@@ -10,4 +11,10 @@ Jids(
     input  = 'scripts/source/chicago-author-date.csl', 
     journal = "Journal of International Development Studies", 
     suffix = {"id": "jids-ja", "title":"JIDS　（日本語）"}
+).create()
+
+Kyosei(
+    input  = 'scripts/source/chicago-author-date.csl', 
+    journal = "Journal of Kyosei Studies", 
+    suffix = {"id": "kyosei-ja", "title":"KYOSEI　（日本語）"}
 ).create()
---
+++ scripts/kyosei.py
@@ -0,0 +1,90 @@
+from .chicago import Chicago
+
+class Kyosei(Chicago):
+    def __init__(self, input, journal, suffix):
+        super().__init__(input, journal, suffix)
+    
+    def custom(self):
+        # remove comma after name (setcitation)
+        self.setattr(self.citation, "z:layout/z:group/z:choose/z:if/z:group/z:text[@macro='contributors-short']", {"suffix":""})
+        
+        # Add pp to page
+        c = self.conds["locators-article"]
+        self.setattr(c["else"], "z:text", {"prefix": ", pp. ", "suffix": ""})
+        
+        # Issue number
+        aj = self.conds["locators"]
+        self.setattr(aj["else"], "group/z:choose/z:if[@variable='volume']/z:group/z:choose/z:if[@variable='issue']/z:text", {"prefix": ", No. ", "suffix": None})
+        self.setattr(aj["else"], "group/z:choose/z:if[@variable='volume']/z:group/z:choose/z:else/z:date", {"prefix": ", (", "suffix": ")"})
+        
+        # Issue number [ja]
+        self.setattr(aj["if"], "group/z:choose/z:if[@variable='volume']/z:text", {"prefix": "第", "suffix": "巻"})
+        self.setattr(aj["if"], "group/choose/if/z:text", {"prefix": "第", "suffix": "号"})
+        self.setattr(aj["if"], "group", {"delimiter": "、"})
+
+        # Japanese dot at the end [ja & en]
+        #remove general suffix
+        self.setattr(self.bibliography, "z:layout", {"suffix": None})
+        text = self.render({"tag": "text"}, self.bibliography, path="z:layout")
+        c = self.addcondition(text, ".")
+        self.setattr(c["if"], "text", {"value": "。"})
+        self.setattr(c["else"], "text", {"value": "."})
+        
+        # remove dot prefix in access
+        self.setattr(self.bibliography, "z:layout/z:text[@macro='access']", {"prefix": None})
+        
+        # add space in front of URL
+        c = self.conds["access"]
+        self.setattr(c["else"], "z:choose/z:if/z:choose/z:if/z:text", {"prefix":". "})
+        self.setattr(c["else"], "z:choose/z:if/z:choose/z:else/z:text", {"prefix":". "})
+
+        # Volume
+        self.setattr(aj["else"], "group/z:choose/z:if[@variable='volume']/z:text[@variable='volume']", {"prefix": " Vol. ", "suffix": None})
+        self.setattr(aj["else"], "group/z:choose/z:if[@variable='volume']/z:group", {"prefix": None, "suffix": None})
+        
+        # Title
+        t = self.conds["title"] 
+        self.setattr(t["else"], "z:text", {"quotes":"false", "prefix": "“", "suffix":"”"})
+        
+        # Journal name
+        t = self.conds["container-title"]
+        self.setattr(t["else"], "z:group/choose/if/z:text", {"prefix":" ", "suffix":". "})
+        
+        # webpage container title [ja]
+        ctw = self.conds["container-title-webpage"]
+        self.setattr(ctw["if"], "z:text", {"prefix":"、", "suffix":"。"})
+
+        # Journal name (Add comma in front) [ja]
+        t = self.conds["container-title"]
+        self.setattr(t["if"], "z:group/choose/if/z:text", {"prefix":"、『"})
+        
+        # Date no parentheses
+        c = self.conds["date"]
+        self.setattr(c["else"], "z:group/z:date[@variable='original-date']", {"prefix": " ", "suffix": ". "})
+        self.setattr(c["else"], "z:group/z:date[@variable='issued']", {"prefix": " ", "suffix": ". "})
+        
+        # Date with comma [ja]
+        self.setattr(c["if"], "z:group/z:date[@variable='original-date']", {"prefix": " ", "suffix": ""})
+        self.setattr(c["if"], "z:group/z:date[@variable='issued']", {"prefix": " ", "suffix": ""})
+        
+        # Bibliography author
+        c = self.conds["contributors"]
+        self.setattr(c["if"], "z:names/z:name", {"name-as-sort-order": None})
+        self.render({"tag": "name-part", "attrib": {"name": "family"}}, c["if"], path="z:names/z:name")
+        self.render({"tag": "name-part", "attrib": {"name": "given", "prefix": " "}}, c["if"], path="z:names/z:name")
+        
+        self.setattr(c["else"], "z:names", {"suffix": ". "})
+        self.setattr(c["else"], "z:names/z:name", {"initialize": "false", "and": "text"})
+        self.setattr(c["else"], "z:names/z:label", {"form": None})
+        
+        # remove dot before "In"
+        c = self.conds["container-contributors"]
+        self.setattr(c["else"], "text", {"prefix": " "})
+        self.setattr(c["else"], "z:group/z:names[@variable='editor translator']/z:name", {"and": "text", "initialize": "false", "name-as-sort-order": "all"})
+        
+        # add 、 in front of contributors [ja]
+        self.setattr(c["if"], "z:group", {"prefix": "、", "suffix": "、"})
+        
+        # add 、 in front of publisher [ja]
+        c = self.conds["publisher"]
+        self.setattr(c["if"], "z:group", {"prefix": "、"})
